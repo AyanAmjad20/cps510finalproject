bcf40575ceb772321380af39598f5f5cc93d39a6
Ayan Amjad <ayanamjad20@gmail.com>
2025-11-10 19:46:50 -0500

    initialize project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  
+
+  return (
+    <>
+      <header> 
+        <h1>Online Movie Store</h1>
+        <p>By Ayan & Alex for CPS510</p>
+      </header>
+      <main>
+        <p>Welcome to the Online Movie Store!</p>
+      </main>
+      <footer>
+        <p>&copy; 2024 Online Movie Store</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
